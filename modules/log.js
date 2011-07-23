c74b4ca3a09d225d5929540b4eafe9c332a92d52
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-24 00:35:33 +0200

    implementing the streaming http connection

--- modules/log.js
+++ modules/log.js
@@ -1,3 +1,5 @@
+var querystring = require('querystring');
+
 this.process = function(request) {
 	if (request.uid && request.lat && request.lon) {
 		request.res.writeHead(200);
@@ -5,6 +7,31 @@ this.process = function(request) {
 		engine.updateUserLocation(request.uid, {lat:request.lat, lon:request.lon});
 		return;
 	}
-	request.res.writeHead(503);
-	request.res.end('log module failed');
+	request.req.on('end', function() {
+		engine.removeUser(request.uid);
+	});
+	request.req.on('close', function() {
+		engine.removeUser(request.uid);
+	});
+	request.req.on('data', function(chunk) {
+		chunk = chunk.toString();
+		if (chunk == 'quit') {
+			console.log('received quit for uid ' + request.uid);
+			engine.removeUser(request.uid);
+			request.res.end("connection closed");
+		} else if (chunk == 'keepalive') {
+			console.log('received keepalive for uid ' + request.uid);
+			engine.keepAliveUser(request.uid);
+		} else {
+			var data = querystring.parse(chunk);
+			if (data.uid) {
+				console.log('identified user: ' + data.uid);
+				request.uid = data.uid;
+			}
+			if (data.lat && data.lon) {
+				console.log('received update for uid ' + request.uid + ': ' + chunk);
+				engine.updateUserLocation(request.uid, data);
+			}
+		}
+	});
 }
