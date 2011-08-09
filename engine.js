9f863c11cab892834f4ce63865013897773d7b6f
Jakob Ketterl <jakob.ketterl@gmx.de>
2011-08-09 07:37:46 +0000

    fixing user timeoutfixing user timeouts

--- engine.js
+++ engine.js
@@ -8,7 +8,6 @@ BGTEngine = function(){
 	var me = this;
 	this.users = [];
 	this.connections = [];
-	this.userTimeouts = {};
 	this.map = BGTMap.getMap(2);
 	this.stats = new BGTStatsEngine(this);
 	this.stats.on('stats', function(stats) {
@@ -45,7 +44,7 @@ BGTEngine.prototype.removeUser = function(user){
 	if (!this.users[user.uid]) return;
 	this.users[user.uid].removeListener('updatelocation', this.onUserUpdate);
 	delete this.users[user.uid];
-	if (this.userTimeouts[user.uid]) clearTimeout(this.userTimeouts[user.uid]);
+	if (user.updateTimeout) clearTimeout(user.updateTimeout);
 	this.sendUpdates(new BGTUpdate('quit', '<user id="' + user.uid + '"/>'));
 }
 
@@ -70,8 +69,8 @@ BGTEngine.prototype.updateUserLocation = function(user, location) {
 
 BGTEngine.prototype.keepAliveUser = function(user) {
 	var me = this;
-	if (this.userTimeouts[user]) clearTimeout(this.userTimeouts[user]);
-	this.userTimeouts[user] = setTimeout(function(){
+	if (user.updateTimeout) clearTimeout(user.updateTimeout);
+	user.updateTimeout = setTimeout(function(){
 		util.log(user + ': update timeout');
 		me.removeUser(user);
 	}, 60000);
