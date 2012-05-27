61d7f86e6d86b80553a6127bcc75234100d61ad9
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-05-27 21:47:06 +0000

    module sub-path logic

--- router.js
+++ router.js
@@ -1,15 +1,16 @@
 var querystring = require('querystring');
 
 BGTRouter = function(){
-	this.regex = new RegExp('^\\/bgt\\/([a-z]+)(\\?(.*))?');
+	this.regex = new RegExp('^\\/bgt\\/([a-z]+)(\\/[a-z]+)*(\\?(.*))?');
 }
 
 BGTRouter.prototype.parse = function(url){
 	var request = {module:'error'};
 	var result = this.regex.exec(url);
 	if (result != null) {
-		var request = querystring.parse(result[3]);
+		request = querystring.parse(result[4]);
 		request.module = result[1];
+		if (result[2]) request.path = result[2];
 	}
 	return request;
 }
