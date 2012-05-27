283fdc43e83595f9a16897dc2782cdab17a951f4
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-05-27 23:32:18 +0000

    output some html

--- ws/js/socket.js
+++ ws/js/socket.js
@@ -1,7 +1,13 @@
 var socket = new WebSocket('wss://' + location.hostname + '/bgt/socket');
+var print = function(text){
+	var div = document.getElementById('events');
+	var el = document.createElement('div');
+	el.innerHTML = text;
+	div.appendChild(el);
+};
 socket.onopen = function(){
-	console.info('connected!');
+	print('socket connected!');
 };
 socket.onmessage = function(message){
-	console.info(message);
+	print('message received: "' + message.data + '"');
 };
