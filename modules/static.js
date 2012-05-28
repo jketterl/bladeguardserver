622b78de3de4d38b7256193444d20048d0c1c149
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-05-28 21:07:06 +0000

    improve directory handling

--- modules/static.js
+++ modules/static.js
@@ -2,21 +2,29 @@ var fs = require('fs');
 
 module.exports.process = function(req){
 	var me = this,
-	    file = __dirname + '/../ws' + req.path;
+	    file = __dirname + '/../ws' + (req.path || '');
 	fs.stat(file, function(err, stats){
 		if (err) switch (err.code) {
 			case 'ENOENT':
+				req.res.writeHead(404);
 				return req.res.end('file not found');
 			default:
+				req.res.writeHead(503);
 				return req.res.end('filesystem error');
 		};
 		if (stats.isDirectory()) {
-			req.path += '/index.html';
-			return me.process(req);
+			if (req.path && req.path.match(/\/$/)) {
+				req.path = (req.path || '' ) + '/index.html';
+				return me.process(req);
+			} else {
+				req.res.writeHead(301, {Location:'/bgt/static' + (req.path || '') + '/'});
+				return req.res.end();
+			}
 		}
 		var stream = fs.createReadStream(file); 
 		stream.on('error', function(err){
 			console.info(err.stack);
+			req.res.writeHead(503);
 			req.res.end('filesystem error');
 		});
 		stream.pipe(req.res);
