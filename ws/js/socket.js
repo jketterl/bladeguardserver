4e8c183501092d37de1d5f7a9cc44c685c4de11c
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-05-28 20:39:56 +0000

    reconnect

--- ws/js/socket.js
+++ ws/js/socket.js
@@ -1,17 +1,27 @@
-var socket = new WebSocket('wss://' + location.hostname + '/bgt/socket');
+var htmlEntities = function(str) {
+	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+};
 var print = function(text){
 	var div = document.getElementById('events');
 	var el = document.createElement('div');
 	el.innerHTML = text;
 	div.insertBefore(el, div.childNodes[0]);
 };
-function htmlEntities(str) {
-	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-socket.onopen = function(){
-	print('socket connected!');
+
+
+var connect = function(){
+	var socket = new WebSocket('wss://' + location.hostname + '/bgt/socket');
+	socket.onopen = function(){
+		print('socket connected!');
+	};
+	socket.onmessage = function(message){
+		console.info(message);
+		print('message received: "' + htmlEntities(message.data) + '"');
+	};
+	socket.onclose = function(){
+		print('socket disconnected! waiting for reconnect...');
+		setTimeout(connect, 5000);
+	}
 };
-socket.onmessage = function(message){
-	console.info(message);
-	print('message received: "' + htmlEntities(message.data) + '"');
-};
+
+connect();
