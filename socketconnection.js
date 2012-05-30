e521aeabb76bf0f8b437d1ca8c5897cb6789ed97
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-05-30 19:02:10 +0000

    add data messaging for upstream

--- socketconnection.js
+++ socketconnection.js
@@ -28,6 +28,7 @@ BGTSocketConnection.prototype.queueUpdates = function(updates){
 BGTSocketConnection.prototype.close = function(){};
 
 BGTSocketConnection.prototype.processMessage = function(message){
+	var me = this;
 	if (message.type != 'utf8') {
 		console.warn('unsupported message type: "' + message.type + '"');
 		return false;
@@ -46,11 +47,47 @@ BGTSocketConnection.prototype.processMessage = function(message){
 		console.warn('unknown command:"' + data.command + '"');
 		return false;
 	}
-	return this['process' + data.command](data.data);
-}
+	return this['process' + data.command](data.data || {}, function(success){
+		var response = {success:true};
+		if (typeof(success) == 'boolean') response.success = success;
+		if (util.isError(success)) {
+			response.success = false;
+			response.data = {
+				message:success.message
+			};
+		}
+		if (data.requestId) response.requestId = data.requestId;
+		me.socket.sendUTF(JSON.stringify(response));
+	});
+};
 
-BGTSocketConnection.prototype.processlog = function(data){
+BGTSocketConnection.prototype.processlog = function(data, callback){
 	this.emit('location', new BGTLocation(data));
+	process.nextTick(callback);
+};
+
+BGTSocketConnection.prototype.processauth = function(data, callback){
+	var me = this;
+	if (!data.user || !data.pass) {
+		util.log('login attempt with missing credentials - denied');
+		return process.nextTick(function(){
+			callback(false);
+		});
+	}
+	BGTUser.login(data.user, data.pass, function(err, user){
+		if (err) {
+			util.log(err);
+			return callback(err);
+		}
+		util.log('user login: ' + user);
+		me.setUser(user);
+		callback(true);
+	});
+};
+
+BGTSocketConnection.prototype.setUser = function(user){
+	this.user = user;
+	return this;
 }
 
 BGTSocketConnection.prototype.getUser = function(){
@@ -59,4 +96,4 @@ BGTSocketConnection.prototype.getUser = function(){
 		util.log('new anonymous user: ' + this.user);
 	}
 	return this.user;
-}
+};
