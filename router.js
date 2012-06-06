5e2f01cfaafba44f9474e67a47f2aa26e5841537
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-06-06 19:06:26 +0000

    modify regex (once again)

--- router.js
+++ router.js
@@ -1,14 +1,14 @@
 var querystring = require('querystring');
 
 BGTRouter = function(){
-	this.regex = new RegExp('^\\/bgt\\/([a-z]+)(\\/[a-z.-_]*)*(\\?(.*))?$');
+	this.regex = /^\/bgt\/([a-z]+)((\/[a-z\.\-_]*)*)(\?(.*))?$/;
 }
 
 BGTRouter.prototype.parse = function(url){
 	var request = {module:'error'};
 	var result = this.regex.exec(url);
 	if (result != null) {
-		request = querystring.parse(result[4]);
+		request = querystring.parse(result[5]);
 		request.module = result[1];
 		if (result[2]) request.path = result[2];
 	}
