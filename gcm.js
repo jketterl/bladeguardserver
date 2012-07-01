ae07705b7a0697f5f56488e38bce0a149ac9b829
Jakob Ketterl <jketterl@chipxonio.de>
2012-07-01 21:51:51 +0200

    fix semicolon

--- gcm.js
+++ gcm.js
@@ -6,7 +6,7 @@ BGT.GCM.Service = function(config){
 };
 
 BGT.GCM.Service.prototype.sendBroadcastMessage = function(message) {
-	var me = this,
+	var me = this;
 	db.query().select('registration_id').from('registration').execute(function(err, result){
 		if (err) returnn util.log('Error querying registrations from the DB\n' + err.stack);
 		var ids = [];
