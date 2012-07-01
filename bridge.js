a37ffbc3d2944bc52aae0b2d2b70e911f62f2365
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-07-01 14:08:57 +0000

    fix bridge details

--- bridge.js
+++ bridge.js
@@ -10,18 +10,24 @@ BGTBridge.Olivier = function(){};
 
 util.inherits(BGTBridge.Olivier, BGTBridge);
 
+// constant password
+BGTBridge.Olivier.password = 'uHHqdERal489A73FiVZxqgbUcBsq6JMKG4QlOYN1smAO59UVtv';
+
 BGTBridge.Olivier.prototype.sendUpdates = function(updates){
 	for (var i in updates) try {
 		var update = updates[i];
 		if (!(update instanceof BGTLocationUpdate)) continue;
 		// all users from olivier are flagged like this
 		if (update.user.foreignServer) return;
-		var location = update.getData().location;
+		var location = update.user.location;
 		var data = {
 			latitude:location.lat,
 			longitude:location.lon,
-			userName:update.user.getHash()
+			userName:update.user.getName(),
+			password:BGTBridge.Olivier.password
+			//team:update.user.getTeam()
 		};
+		if (location.speed) data.speed = location.speed;
 		var req = http.request({
 			host:'ocroquette.fr',
 			path:'/bladenighttracker/userupdate',
--- modules/userupdate.js
+++ modules/userupdate.js
@@ -14,11 +14,12 @@ module.exports.process = function(req){
 			data = JSON.parse(data);
 		} catch (e) {
 			util.log('could not parse json: "' + data + '"');
-			req.res.writeHead(503);
-			return req.res.end('could not parse your data');
+			req.res.writeHead(503, {'content-type':'application/json'});
+			return req.res.end(JSON.stringify({message:'could not parse your data'}));
 		}
-		engine.updateUserLocation(getUser(data.userName), {lat: data.latitude, lon: data.longitude});
-		req.res.end('OK');
+		engine.updateUserLocation(getUser(data.userName), new BGTLocation({lat: data.latitude, lon: data.longitude}));
+		req.res.writeHead(200, {'content-type':'application/json'});
+		req.res.end(JSON.stringify({message:'OK'}));
 	});
 };
 
