db5eeeba02bbe34537927e11e0fa2a101e805db6
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-07-16 15:25:39 +0000

    finally fix that old "updates are coming multiple times" problem

--- engine.js
+++ engine.js
@@ -50,7 +50,7 @@ BGTEngine.prototype.addUser = function(user) {
 
 BGTEngine.prototype.removeUser = function(user){
 	if (!this.users[user.uid]) return;
-	this.users[user.uid].removeListener('updatelocation', this.onUserUpdate);
+	this.users[user.uid].removeListener('locationupdate', this.onUserUpdate);
 	delete this.users[user.uid];
 	if (user.updateTimeout) clearTimeout(user.updateTimeout);
 	this.sendUpdates(new BGTUpdate('quit', {user:{id:user.uid}}));
