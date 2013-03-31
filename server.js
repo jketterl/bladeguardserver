96e4f3f1fe0a56772c8ebe8c85e35204fe9eeb2c
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-03-31 14:15:28 +0000

    add timezone capabilities

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ BGT.messenger = {
 require('./facebook');
 var express = require('express');
 var engine = require('ejs-locals');
+var time = require('time')(Date);
 
 db.connect(function(err){
 	if (err) {
