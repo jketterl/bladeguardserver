d9b4b7ba6d26b6d530d3ba9c312b0c61335c2aaf
Jakob Ketterl <jakob.ketterl@gmx.de>
2014-03-20 22:13:02 +0000

    lots of work for the website reconfigure for stunnel use

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var https = require('https');
+var http = require('http');
 var fs = require('fs');
 require('./engine');
 require('./router');
@@ -14,79 +14,86 @@ BGT = {};
 require('./gcm');
 require('./apns');
 BGT.messenger = {
-	messengers:[
-		new BGT.GCM.Service(require('./config/gcm.json')),
-		new BGT.APNS.Service(require('./config/apns.json'))
-	],
-	sendBroadcastMessage:function(){
-		var a = arguments;
-		this.messengers.forEach(function(messenger){
-			messenger.sendBroadcastMessage.apply(messenger, a);
-		});
-	}
+    messengers:[
+        new BGT.GCM.Service(require('./config/gcm.json')),
+        new BGT.APNS.Service(require('./config/apns.json'))
+    ],
+    sendBroadcastMessage:function(){
+        var a = arguments;
+        this.messengers.forEach(function(messenger){
+            messenger.sendBroadcastMessage.apply(messenger, a);
+        });
+    }
 };
 require('./facebook');
 var express = require('express');
 var engine = require('ejs-locals');
 
 db.connect(function(err){
-	if (err) {
-		util.log('could not connect to database; exiting.');
-		return;
-	}
+    if (err) {
+        util.log('could not connect to database; exiting.');
+        return;
+    }
 
-	BGTEvent.loadAll(function(events){
-		if (util.isError(events)) {
-			util.log('could not load event data from database; exiting.');
-			return;
-		}
+    BGTEvent.loadAll(function(events){
+        if (util.isError(events)) {
+            util.log('could not load event data from database; exiting.');
+            return;
+        }
 
-		var startServer = function(options){
-			var app = express();
-			app.engine('ejs', engine);
-			app.set('view engine', 'ejs');
-			app.get('/', function(req, res){
-				res.render('index');
-			});
-			app.use('/static', express.static(__dirname + '/ws'));
-			app.get('/event.html', function(req, res){
-				res.render('event/list', {events:BGTEvent.getAll(true)});
-			});
-			app.get('/event/:id.html', function(req, res){
-				res.render('event/event', {
-					event:BGTEvent.get(req.params.id),
-					url:'https://' + req.headers.host + '/event/' + req.params.id + '.html'
-				});
-			});
-			app.get('/admin', function(req, res){
-				res.render('admin/index');
-			});
+        var startServer = function(options){
+            var app = express();
+            app.engine('ejs', engine);
+            app.locals.dateformat = require('dateformat');
+            app.set('view engine', 'ejs');
+            app.get('/', function(req, res){
+                var events = BGTEvent.getAll();
+                res.render('index', { nextEvent: events[0] });
+            });
+            app.use('/static', express.static(__dirname + '/ws'));
+            app.get('/event.html', function(req, res){
+                res.render('event/list', {
+                    events:BGTEvent.getAll(true),
+                    year:parseInt(req.query.year) || new Date().getFullYear()
+                });
+            });
+            app.get('/download.html', function(req, res){ res.render('download'); });
+            app.get('/event/:id.html', function(req, res){
+                res.render('event/event', {
+                    event:BGTEvent.get(req.params.id),
+                    url:'https://' + req.headers.host + '/event/' + req.params.id + '.html'
+                });
+            });
+            app.get('/admin', function(req, res){
+                res.render('admin/index');
+            });
+            app.get('/impressum.html', function(req, res) { res.render('impressum'); } );
 
-			var httpServer = https.createServer(options, app).listen(443)
+            var httpServer = http.createServer(app).listen(3000)
 
-			var wsServer = new WebSocketServer({
-				httpServer:httpServer,
-				maxReceivedFrameSize:1024*1024
-			});
+            var wsServer = new WebSocketServer({
+                httpServer:httpServer,
+                maxReceivedFrameSize:1024*1024
+            });
 
-			wsServer.on('request', function(request){
-				var connection = new BGTSocketConnection(request.accept());
-			});
-		};
+            wsServer.on('request', function(request){
+                var connection = new BGTSocketConnection(request.accept());
+            });
+        };
 
-		var options = require('./config/keys.json');
-		var callbacks = 0;
-		for (var a in options) {
-			callbacks ++;
-			(function(a){
-				fs.readFile(options[a], function(err, data){
-					if (err) throw err;
-					callbacks--;
-					options[a] = data.toString();
-					if (callbacks == 0) startServer(options);
-				});
-			})(a);
-		}
+        var options = require('./config/keys.json');
+        var callbacks = 0;
+        for (var a in options) {
+            callbacks ++;
+            (function(a){
+                fs.readFile(options[a], function(err, data){
+                    if (err) throw err;
+                    callbacks--;
+                    options[a] = data.toString();
+                    if (callbacks == 0) startServer(options);
+                });
+            })(a);
+        }
 
-	});
+    });
 });
