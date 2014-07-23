9e12eed09048799eb22ae5f808f94ee670ff821d
Jakob Ketterl <jakob.ketterl@gmx.de>
2014-07-23 18:50:58 +0000

    catch errors during weather parsing

--- weather/yahoo.js
+++ weather/yahoo.js
@@ -29,26 +29,31 @@ YahooWeatherProvider.prototype.getPrognosis = function(d, c) {
         var data = '';
         res.on('data', function(chunk) { data += chunk });
         res.on('end', function() {
-            xml2js.parseString(data, function(err, result) {
-                var w = null
-                result.rss.channel[0].item[0]['yweather:forecast'].forEach(function(x){
-                    var props = x['$'];
-                    if (moment(props.date).isSame(d, 'day')) {
-                        w = props;
-                        w.text = codes[w.code];
-                        w.image = "/weather/icons/" + w.code + ".png";
-                    }
+            try {
+                xml2js.parseString(data, function(err, result) {
+                    var w = null
+                    result.rss.channel[0].item[0]['yweather:forecast'].forEach(function(x){
+                        var props = x['$'];
+                        if (moment(props.date).isSame(d, 'day')) {
+                            w = props;
+                            w.text = codes[w.code];
+                            w.image = "/weather/icons/" + w.code + ".png";
+                        }
 
+                    });
+                    if (w != null) me.cache[ck] = {
+                        validUntil:moment(now).add(1, 'hours'),
+                        value:w
+                    };
+                    c(w);
                 });
-                if (w != null) me.cache[ck] = {
-                    validUntil:moment(now).add(1, 'hours'),
-                    value:w
-                };
-                c(w);
-            });
+            } catch (e) {
+                console.warn('could not parse weather data:\n' + e + '\n' + data);
+                c(null);
+            }
         })
     }).on('error', function(err) {
-        c(null)
+        c(null);
     });
 };
 
